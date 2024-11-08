a2fed63c103cdb91b8e9dd58dd13a0e5e93ef089
makkiboukhris <makki01boukhris@gmail.com>
2024-11-08 15:17:53 +0100

    added the boxes in overview section

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 // Importing the components we’ll create below
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
+import Overview from './components/Dashboard/Overview';
 
 const App: React.FC = () => (
-  <div className="h-screen flex flex-col bg-black">
-    <Header /> {/* Header at the top taking full width */}
+  <div className="h-screen flex flex-col bg-[#D9D9D9]">
+    <Header /> 
     <div className="flex flex-1">
-      <Sidebar /> {/* Sidebar under the header */}
+      <Sidebar /> 
+      <div className="flex-1 p-4"> 
+        <Overview /> 
+      </div>
     </div>
   </div>
 );
---
+++ src/components/Dashboard/Overview.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const OverviewSection: React.FC = () => {
+  return (
+    <div className="flex flex-wrap gap-4 p-4 justify-center">
+      <div className="bg-white shadow-md rounded-lg h-96 min-w-[500px] flex-grow p-4">
+        {/* Placeholder for Status of charging stations */}
+      </div>
+      <div className="bg-white shadow-md rounded-lg h-96 min-w-[500px] flex-grow p-4">
+        {/* Placeholder for Status of the fleet */}
+      </div>
+      <div className="bg-white shadow-md rounded-lg h-96 min-w-[500px] flex-grow p-4">
+        {/* Placeholder for Driving schedule */}
+      </div>
+      <div className="bg-white shadow-md rounded-lg h-96 min-w-[500px] flex-grow p-4">
+        {/* Placeholder for Charging schedule */}
+      </div>
+    </div>
+  );
+};
+
+export default OverviewSection;
