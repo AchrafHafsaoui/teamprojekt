b07cee29d698efb95964f776ca2713366addda5e
azizmokhtar <mohamedazizelmokhtar27@gmail.com>
2024-11-11 22:22:16 +0100

    Minor sidebar and header changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import userIcon from '../assets/icons/menuLogo.svg'; 
 
 const Header: React.FC = () => (
-  <div className="flex justify-end items-center bg-white p-1 border-none fixed left-0 right-0 z-0 shadow-[rgba(0,0,15,0.1)_0px_1px_10px_0px] h-12">
-    <div className="flex justify-between mx-15 text-lg items-center">
-    <p className='mx-10 hover:underline'>settings</p>
-    <p className='mx-10 hover:underline'>account</p>
-    <p className='mx-10 hover:underline'>support</p>
-    </div>
-    <img 
-      src={userIcon} 
-      alt="User Icon" 
-      className="bg-white mx-10 w-8 h-8" 
-    />
+  <div className="flex justify-end items-center bg-white p-1 border-none fixed left-0 right-0 z-0  h-20">
+    <button className="flex justify-between mx-8 text-2xl items-center">
+      <p className='mx-10 hover:underline'>settings</p>
+      <p className='mx-10 hover:underline'>account</p>
+      <p className='mx-10 hover:underline'>support</p>
+    </button>
+    <button>
+      <img 
+        src={userIcon} 
+        alt="User Icon" 
+        className="bg-white mx-10 w-8 h-8" 
+      />
+    </button>
   </div>
 );
 
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -24,14 +24,14 @@ const Sidebar: React.FC = () => {
   ];
 
   return (
-    <div className="flex flex-col bg-[#F1F1F1]  text-white w-20 hover:w-60 hover:shadow-[rgba(0,0,15,0.1)_4px_0px_4px_0px] duration-300 h-screen fixed justify-between">
-      <div className="flex flex-col space-y-2">
-        <img src={logo} className="w-12 mx-4"></img>
+    <div className="flex flex-col bg-[#F1F1F1]  w-20 hover:w-80 hover:shadow-[rgba(0,0,15,0.1)_4px_0px_4px_0px] duration-300 h-screen fixed justify-between">
+      <div className="flex flex-col space-y-2 mt-4">
+        <img src={logo} className="w-12 mx-auto"></img>
         {menuItems.map((item) => (
           <button
             key={item.label}
             onClick={() => setActiveButton(item.label)}
-            className="flex items-center p-2 rounded hover:bg-[#C0C0C0] transition duration-300 relative"
+            className="flex items-center p-2 rounded hover:bg-[#C0C0C0] hover:ml-3   transition duration-300 relative"
           >
             <img src={item.icon} alt={item.label} className="w-10 mx-4" />
             <span
@@ -42,7 +42,7 @@ const Sidebar: React.FC = () => {
             </span>
             {/* Right side black line for active button */}
             {activeButton === item.label && (
-              <div className="absolute right-0 top-0 h-full w-1 bg-black rounded-l"></div>
+              <div className="absolute right-0 top-0 h-full w-1 bg-black rounded"></div>
             )}
           </button>
         ))}
