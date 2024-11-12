7478fb93cb3f14cf2864fa68159acf49dd7b72c6
makkiboukhris <makki01boukhris@gmail.com>
2024-11-12 15:51:51 +0100

    updated the header button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,22 +6,18 @@ const Header: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return(
   <div className="flex justify-end items-center bg-white p-1 border-none fixed left-0 right-0 z-0  h-20">
-    <div className={`transition-transform duration-500 ease-in-out ${isMenuOpen ? ' translate-x-0 opacity-100' : ' translate-x-1/4 opacity-0'}`}>
-  {
-    isMenuOpen&&(
+    <div className={`transition-all transform duration-500  ${isMenuOpen ? ' translate-x-0 opacity-100' : ' translate-x-20 opacity-0'}`} style={{ visibility: isMenuOpen ? 'visible' : 'hidden' }}>
       <div className="flex justify-between mx-8 text-2xl items-center">
-      <p className='mx-10 hover:underline hover:cursor-pointer'>settings</p>
-      <p className='mx-10 hover:underline hover:cursor-pointer'>account</p>
-      <p className='mx-10 hover:underline hover:cursor-pointer'>support</p>
+      <p className="mx-10 hover:underline hover:cursor-pointer">settings</p>
+      <p className="mx-10 hover:underline hover:cursor-pointer">account</p>
+      <p className="mx-10 hover:underline hover:cursor-pointer">support</p>
     </div> 
-    )
-  }
   </div>
-    <button onClick={()=>setIsMenuOpen(!isMenuOpen)}>
+    <button onClick={()=>setIsMenuOpen(!isMenuOpen)} className={`mr-10 rounded-full p-3 ${isMenuOpen ? ' bg-[#d6d6d682] backdrop-blur-sm' : ' bg-white'}` } >
       <img 
         src={userIcon} 
         alt="User Icon" 
-        className="bg-white mx-10 w-8 h-8" 
+        className=' w-8 h-8 '
       />
     </button>
   </div>
